1c1ca2c4c6468b73d70781de10875b27e37b811a
Andre LeBlanc <andre.leblanc88@gmail.com>
2016-08-29 15:37:54 -0400

    feat: most recent segment metadata

--- spec/support/shared_examples/for_query_datasource.rb
+++ spec/support/shared_examples/for_query_datasource.rb
@@ -15,6 +15,16 @@ RSpec.shared_examples 'for_query_datasource' do
     end
   end
 
+  describe '#datasource_metadata' do
+    let(:datasource_name) { 'widgets' }
+    let(:query_args) { { "queryType" => "dataSourceMetadata", "dataSource" => datasource_name } }
+
+    it 'delegates call' do
+      expect(subject.broker).to receive(:query).with(query_args)
+      subject.datasource_metadata(datasource_name)
+    end
+  end
+
   describe '#disable_datasource' do
     it 'delegates call' do
       expect(subject.coordinator).to receive(:disable_datasource)
@@ -65,6 +75,40 @@ RSpec.shared_examples 'for_query_datasource' do
       expect(subject).to receive(:list_datasources).and_return(datasources)
       expect(subject).to receive(:delete_datasource).exactly(datasources_count).times
       subject.delete_datasources
+    end
+  end
+
+  describe '#most_recent_segment_metadata' do
+    let(:datasource_metadata) { JSON.generate(datasource_metadata_hash) }
+    let(:datasource_metadata_hash) {
+      [
+        {
+          "timestamp" => "2016-08-29T17:42:17.000Z",
+          "result" => { "maxIngestedEventTime" => "2016-08-29T17:42:17.000Z" }
+        }
+      ]
+    }
+
+    it 'returns the most recent segment metadata' do
+      expect(subject.broker).to receive(:query).twice.and_return(datasource_metadata, nil)
+      subject.most_recent_segment_metadata('foo')
+    end
+  end
+
+  describe '#segment_metadata' do
+    let(:query) {
+      {
+        "queryType" => "segmentMetadata",
+        "dataSource" => datasource,
+        "intervals" => intervals
+      }
+    }
+    let(:datasource) { 'foo' }
+    let(:intervals) { ['2016-08-01/2016-08-30'] }
+
+    it 'delegates call' do
+      expect(subject.broker).to receive(:query).with(query)
+      subject.segment_metadata(datasource, intervals)
     end
   end
 end
